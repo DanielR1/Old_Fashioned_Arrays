7e612801e54d6134551f738c151a9c71c70c1312
DavidAndBlake <league@WTS5.attlocal.net>
2018-06-21 20:01:21 -0700

    hj
    j

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -3,5 +3,39 @@ package _01_array_manipulation;
 
 
 public class Inserting {
+
+	public static int[] insertAt(int[] testArray, int i, int j) {
+		// TODO Auto-generated method stub
+	int[] a = new int[testArray.length+1];
+	for (int k = 0; k < i; k++) {
+		a[k]=testArray[k];
+	}
+	a[i]=j;
+	for (int k = i+1; k < testArray.length+1; k++) {
+		a[k]=testArray[k-1];
+	}
+		return a;
+	}
+
+	public static String[] insertAlphabetically(String[] orderedArray, String string) {
+		// TODO Auto-generated method stub
+		String[] a = new String[orderedArray.length+1];
+		for (int k = 0; k < orderedArray.length; k++) {
+			a[k]=orderedArray[k];
+		}
+		a[a.length-1]=string;
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length-1; j++) {
+			if(a[j].compareTo(a[j+1])>0) {
+				String temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+				
+			}
+			}
+		}
+		
+		return a;
+	}
 	
 }
--- src/_02_diy_array_list/ArrayList.java
+++ src/_02_diy_array_list/ArrayList.java
@@ -1,5 +1,42 @@
 package _02_diy_array_list;
 
-public class ArrayList{
+public class ArrayList <T extends Object>{
+Object[] a = new Object[0]; 
+	public void add(T object) {
+		// TODO Auto-generated method stub
+		Object[] b=new Object[a.length+1];
+		for (int i = 0; i < a.length; i++) {
+			b[i]=a[i];
+		}
+		b[a.length]=object;
+		a=b;
+		
+		
+	}
+
+	public T get(int i) {
+		// TODO Auto-generated method stub
+		return (T) a[i];
+	}
+
+	public void set(int i, T object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addAt(int i, T object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void remove(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean contains(T object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
